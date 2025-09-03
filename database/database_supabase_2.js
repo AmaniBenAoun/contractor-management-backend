7e560a616bf81dd8e94f3dd4d55009b3750b3b39
Amani Benaoun <amani.benaoun@median-kliniken.de>
2025-09-03 19:57:38 +0200

    Add connection pool settings to Sequelize
    Updated Sequelize configuration to include connection pool settings.

--- database/database_supabase_2.js
+++ database/database_supabase_2.js
@@ -1,9 +1,15 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
+  dialect: 'postgres',
   dialectOptions: {
     ssl: { require: true, rejectUnauthorized: false },
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+    acquire: 30000,
   },
   logging: false,
 });
